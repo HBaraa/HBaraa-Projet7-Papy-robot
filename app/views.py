7c937733dd058fe0b0a98e8f977bd9f96f9cd9aa
Baraa Hamdi <hamdibaraa42@gmail.com>
2021-10-27 22:59:33 +0200

    first commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+﻿from flask import render_template, jsonify, request
+from . import app
+
+@app.route("/")
+@app.route("/home.html/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/ajax", methods=["POST"])
+def ajax():
+    user_text= request.form["userText"]
+    print(user_text)
+    return jsonify(["pas de réponse"])
