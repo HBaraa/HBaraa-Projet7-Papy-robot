23aeae826b76217b64f7440a0e5c947916bd4896
Baraa Hamdi <hamdibaraa42@gmail.com>
2021-10-28 16:47:48 +0200

    adding a page html

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-﻿from flask import render_template, jsonify
+﻿from flask import render_template, jsonify, request
 from . import app
 
 @app.route("/")
@@ -7,6 +7,13 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/presentation")
+def presentation():
+    return render_template("presentation.html")
+
+
 @app.route("/ajax", methods=["POST"])
 def ajax():
+    user_text = request.form["question"]
+    print("la requéte est : ", user_text)
     return jsonify(["pas de réponse"])
